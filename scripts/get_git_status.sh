6d4dc0339b09d516256f0b86f90798d67ee342c5
heyyou <hey3you3@gmail.com>
2020-02-12 01:50:34 +0900

    fix: display git status

--- scripts/get_git_status.sh
+++ scripts/get_git_status.sh
@@ -5,11 +5,11 @@ cd $PANE_PATH
 
 get_git_status() {
   local branch="$(git rev-parse --abbrev-ref HEAD)"
-  local change_st="$(git status --short | perl -nlE 'if(/^ M/){$n+=1} END{say "M:$n"}')"
-  local add_st="$(git status --short | perl -nlE 'if(/\?\?/){$n+=1} END{say "??:$n"}')"
+  local change_st="$(git status --short | perl -nlE 'if(/^ M/){$n+=1}; END{if($n==0){$n=0} say "M:$n"}')"
+  local add_st="$(git status --short | perl -nlE 'if(/\?\?/){$n+=1} END{if($n==0){$n=0} say "??:$n"}')"
   local remote_st="$(git rev-list --count --left-right HEAD...@{upstream} 2>/dev/null | perl -anlE 'say "+@F[0]-@F[1]"')"
 
-  printf "[$branch] [$change_st$add_st] [$remote_st]"
+  printf "[$branch] [$change_st $add_st] [$remote_st]"
 }
 
 main() {
