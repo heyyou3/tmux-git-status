76744e2c7369314d4af0903ef051d9091117df1f
heyyou <hey3you3@gmail.com>
2020-02-12 02:25:09 +0900

    fix: Fixed condition to display git staging status

--- scripts/get_git_status.sh
+++ scripts/get_git_status.sh
@@ -6,7 +6,7 @@ cd $PANE_PATH
 get_git_status() {
   local branch="$(git rev-parse --abbrev-ref HEAD)"
   local change_st="$(git status --short | perl -nlE 'if(/^ M/){$n+=1}; END{if($n==0){$n=0} say "M:$n"}')"
-  local stage_st="$(git status --short | perl -nlE 'if(/M/){$n+=1}; END{if($n==0){$n=0} say "S:$n"}')"
+  local stage_st="$(git status --short | perl -nlE 'if(/^M/){$n+=1}; END{if($n==0){$n=0} say "S:$n"}')"
   local add_file_st="$(git status --short | perl -nlE 'if(/\?\?/){$n+=1} END{if($n==0){$n=0} say "??:$n"}')"
   local remote_st="$(git rev-list --count --left-right HEAD...@{upstream} 2>&1 | perl -anlE 'if(@F[0]=~/^[0-9]+$/ && @F[1]=~/^[0-9]+$/){say "+@F[0]-@F[1]"}else{say "+0-0"}')"
 
