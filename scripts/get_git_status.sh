0fe94d5f1aae617e6a3a3d78f4217b33d5fab298
heyyou <hey3you3@gmail.com>
2020-02-12 13:10:47 +0900

    fix: Fixed wrong number of staging files

--- scripts/get_git_status.sh
+++ scripts/get_git_status.sh
@@ -6,11 +6,11 @@ cd $PANE_PATH
 get_git_status() {
   local branch="$(git rev-parse --abbrev-ref HEAD)"
   local change_st="$(git status --short | perl -nlE 'if(/^ M/){$n+=1}; END{if($n==0){$n=0} say "M:$n"}')"
-  local stage_st="$(git status --short | perl -nlE 'if(/^M/){$n+=1}; END{if($n==0){$n=0} say "S:$n"}')"
   local add_file_st="$(git status --short | perl -nlE 'if(/\?\?/){$n+=1} END{if($n==0){$n=0} say "??:$n"}')"
+  local stage_st="$(git diff --staged --name-only | perl -nlE 'END{say $.}')"
   local remote_st="$(git rev-list --count --left-right HEAD...@{upstream} 2>&1 | perl -anlE 'if(@F[0]=~/^[0-9]+$/ && @F[1]=~/^[0-9]+$/){say "+@F[0]-@F[1]"}else{say "+0-0"}')"
 
-  printf "[$branch] [$change_st $stage_st $add_file_st] [$remote_st]"
+  printf "[$branch] [$change_st $add_file_st $stage_st] [$remote_st]"
 }
 
 main() {
